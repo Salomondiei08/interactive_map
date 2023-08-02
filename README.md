e3c254f8ac3cbbf7e468ff369439cd54fb97eec0
Salomon Diei <salomondiei08@gmail.com>
2023-08-02 11:34:31 +0000

    Project completed

--- README.md
+++ README.md
@@ -1,30 +1,33 @@
 # Interactive Map Application
 
-This is an interactive map application built using Leaflet, a powerful open-source JavaScript library for mobile-friendly interactive maps. The application has several interactive features such as adding markers, fixing marker positions, and deleting markers all in a user-friendly interface powered by bootstrap.
+This is an interactive map application built using Leaflet, a powerful open-source JavaScript library for mobile-friendly interactive maps. The application has several interactive features such as adding markers, fixing marker positions, and deleting markers all in a user-friendly interface powered by Bootstrap.
 
 ## Features
 
 - **Add Marker:** Enables users to add a new marker on the map.
 - **Set Position:** Once a marker is added, users have the option to fix its position on the map. Once the position is set, it can't be moved anymore.
+- **Display Coordinates:** Once a marker's position is set, users can click on the marker to display a popup that has information on the coordinates of the marker. The popup will show the latitude and longitude of the marker's fixed position.
 - **Delete Markers:** Any marker added on the map can be deleted using a ‘Delete’ button from a side panel.
 - **Side Panel:** The side panel displays a list of all the added markers with their coordinates, along with a delete button for each marker.
 
 ## Screenshots
 
-[Add screenshots of your app here]
+![Screenshot Without Pointers](images/screen-without-marker.png)
+![Screenshot With Pointers](images/screen-with-markers.png)
 
 ## Getting Started
 
 ### Prerequisites
 
-- A web browser (Chrome, Firefox, Safari etc.)
+- A web browser (Chrome, Firefox, Safari, etc.)
 
 ### Installation 
 
 No installation required. You can use the web application directly.
 
 1. Clone the repository or download the code.
-2. Open the `index.html` file in your browser.
+2. Enter in the src folder
+3. Open the `index.html` file in your browser.
 
 ## Usage
 
@@ -39,10 +42,8 @@ No installation required. You can use the web application directly.
 
 ## Author
 
-- **Salomon DIEI**: Mobile and Web Developper
+- **Salomon DIEI**: Mobile and Web Developer
 
-
-  
 ## Acknowledgments 
 
 The map data is provided by OpenStreetMap contributors.
--- src/script.js
+++ src/script.js
@@ -1,16 +1,20 @@
 // Initialization
-var map = L.map("map").setView([51.505, -0.09], 13);
+// Create a Leaflet map with a default view centered at latitude 5.359952 and longitude -4.008256 with zoom level 12.
+var map = L.map("map").setView([5.359952, -4.008256], 12);
+
+// Add an OpenStreetMap tile layer to the map with attribution to Salomon DIEI.
 L.tileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   {
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">Salomon DIEI</a>',
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">Salomon DIEI</a>',
   }
 ).addTo(map);
 
+// Initialize arrays and a map to store markers' data and objects.
 var markersData = [];
 var markerObjects = new Map();
 
+// Function to update the side panel with the list of markers.
 function updateSidePanel() {
   var sidePanel = document.querySelector('.sidebar-panel');
   sidePanel.innerHTML = '';
@@ -18,6 +22,7 @@ function updateSidePanel() {
   var list = document.createElement('ul');
   list.className = 'list-group';
 
+  // Loop through each marker's data and create list items with delete buttons.
   markersData.forEach(function (markerData, index) {
     var listItem = document.createElement('li');
     listItem.className = 'list-group-item d-flex justify-content-between align-items-center';
@@ -26,6 +31,8 @@ function updateSidePanel() {
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'btn-primary rounded-button';
+
+    // Add a click event listener to the delete button to remove the marker from the map and the data arrays.
     deleteButton.addEventListener('click', function() {
       map.removeLayer(markerObjects.get(markerData.id));
       markerObjects.delete(markerData.id);
@@ -40,19 +47,21 @@ function updateSidePanel() {
   sidePanel.appendChild(list);
 }
 
-var newMarker;
+// Add a marker on the Map
 document.getElementById('add-marker').addEventListener('click', function () {
-  newMarker = L.marker([51.505, -0.09], {
+  // Create a new draggable marker on the map.
+  newMarker = L.marker([5.359952, -4.008256], {
     draggable: true,
     autoPan: true,
-  })
-  .addTo(map);
+  }).addTo(map);
 
+  // Add a dragend event listener to update the marker's position in the markersData array when it is dragged.
   newMarker.on('dragend', function () {
     markersData.find(marker => marker.id === newMarker._leaflet_id).latlng = newMarker.getLatLng();
     updateSidePanel();
   });
 
+  // Store the new marker's data and object in their respective arrays and update the side panel.
   markersData.push({
     id: newMarker._leaflet_id,
     latlng: newMarker.getLatLng()
@@ -60,14 +69,19 @@ document.getElementById('add-marker').addEventListener('click', function () {
   markerObjects.set(newMarker._leaflet_id, newMarker);
   updateSidePanel();
 
+  // Enable/disable buttons to manage marker addition.
   document.getElementById('set-marker').disabled = false;
   document.getElementById('add-marker').disabled = true;
 });
 
+// Set Marker Position
 document.getElementById('set-marker').addEventListener('click', function () {
+  // If a new marker exists, disable dragging, bind a popup with coordinates, and reset the newMarker variable.
   if (newMarker) {
     newMarker.dragging.disable();
+    newMarker.bindPopup("Coordinates: " + newMarker.getLatLng());
     newMarker = null;
+    // Enable/disable buttons to manage marker addition.
     document.getElementById('set-marker').disabled = true;
     document.getElementById('add-marker').disabled = false;
   }
